db451d3b34af6e58ed0e1bbd175a4be5c1a554e8
PGAitor <pg.aitor@gmail.com>
2021-11-05 10:08:01 +0100

    js why do you not push?

---
+++ js/ImgNameChanger.js
@@ -0,0 +1,16 @@
+let i = true;
+let a = document.getElementById('imagen');
+function fot () {
+  if (i == true) {
+    i = false;
+    a.style.backgroundImage = "url('img/mini.png')";
+    a.style.backgroundPosition = 'top';
+    a.style.filter = 'saturate(2)';
+  }
+  else {
+    i = true;
+    a.style.backgroundImage = "url('img/crash2.jpeg')";
+    a.style.backgroundPosition = 'center';
+    a.style.filter = 'none';
+  }
+}
